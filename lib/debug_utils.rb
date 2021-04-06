7d1838847a984a5ce1ec8069526903b2549cafa2
mnbi <mnbi@users.noreply.github.com>
2021-04-06 14:15:13 +0900

    fix: modify to call super in included/extended hooks

--- lib/debug_utils.rb
+++ lib/debug_utils.rb
@@ -5,10 +5,12 @@ module DebugUtils
   # module hooks
 
   def included(mod)
+    super
     puts "- INCLUDED: #{mod} < #{self}"
   end
 
   def extended(mod)
+    super
     puts "- EXTENDED: #{mod} -> #{self}"
   end
 
