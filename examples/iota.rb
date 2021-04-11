22151166ce8b0d4329e7caf8ba3d2f68fd3d497d
mnbi <mnbi@users.noreply.github.com>
2021-04-11 11:16:32 +0900

    feat: rewrite example/iota.rb

--- examples/iota.rb
+++ examples/iota.rb
@@ -9,13 +9,48 @@ if Rus3.debug_mode?
   pp self.class.included_modules
 end
 
-def iota_int(count, start = 0, step = 1)
+# (define (iota count start step)
+#   (if (zero? count)
+#       ()
+#       (cons start (iota (- count 1) (+ start step) step))))
+def iota(count, start = 0, step = 1)
   if zero?(count)
     EMPTY_LIST
   else
-    cons(start, iota_int(count - 1, start + step, step))
+    cons(start, iota(count - 1, start + step, step))
   end
 end
 
-l0 = iota_int(5)
+l0 = iota(5)
 display(l0)
+
+l1 = iota(10, 3.14, 0.01)
+display(l1)
+
+l2 = iota(20, Rational(1, 9), Rational(1, 99))
+display(l2)
+
+# (define (iota-tr count start step)
+#   (iota-tr-iter count () start step))
+#
+# (define (iota-tr-iter result count start step)
+#   (if (zero? count)
+#       result
+#       (iota-tr-iter (append result (list start))
+#                     (- count 1)
+#                     (+ start step)
+#                     step)))
+def iota_tr(count, start = 0, step = 1)
+  iota_tr_iter(EMPTY_LIST, count, start, step)
+end
+
+def iota_tr_iter(result, count, start, step)
+  if zero?(count)
+    result
+  else
+    iota_tr_iter(append(result, list(start)), count - 1, start + step, step)
+  end
+end
+
+l3 = iota_tr(30)
+display(l3)
