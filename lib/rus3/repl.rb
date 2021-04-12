91f71c0eb07ce0d6a9d04a44df91e7bb8a9b8fe0
mnbi <mnbi@users.noreply.github.com>
2021-04-12 20:13:59 +0900

    feat: enable to use local variables in the REPL

--- lib/rus3/repl.rb
+++ lib/rus3/repl.rb
@@ -6,65 +6,29 @@ module Rus3
   #
   # Features:
   #
-  #   `_`            : Refers the last evaluated value.
-  #   `_history(n)`  : Refers the n-th entry of the history.
-  #   `_history`     : shows all entries in the history.
-  #   `_his` or `_h` : an alias of `history`
-  #
   # Short usage:
   #
   #   require "rus3"
   #   include Rus3
   #
-  #   repl = Repl.new
+  #   repl = Repl.new(binding)
   #   repl.start
 
   class Repl
+
     # Indicates the version of the Repl class.
     VERSION = "0.1.0"
 
     include EmptyList
 
-    def initialize
-      @history = []
+    def initialize(bind)
+      @binding = bind
       greeting
     end
 
-    # Returns the last value evaluated by "eval_exp".
-    def last_value
-      @history[-1]
-    end
-    alias :_ :last_value
-
-    # Saves the last value evaluated by "eval_exp"
-    def save_last_value(value)
-      unless UNDEF == value or @history[-1] == value
-        @history << value unless UNDEF == value
-      end
-    end
-
-    # Shows history entries.
-    def _history(num = nil)
-      unless num.nil?
-        @history[num]
-      else
-        if @history.empty?
-          puts "(no entry)"
-        else
-          @history.each_with_index { |e, i|
-            print "#{i}: "
-            pretty_print(e)
-          }
-        end
-        UNDEF
-      end
-    end
-    alias :_his :_history
-    alias :_h :_history
-
     # Evaluates an expression.
     def eval_exp(exp)
-      eval(exp)
+      @binding.eval(exp)
     end
 
     # Starts REPL.
@@ -85,7 +49,6 @@ module Rus3
           next
         end
 
-        save_last_value(v)
         print "==> "
         pretty_print(v)                     # PRINT
       }
